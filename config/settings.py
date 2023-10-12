f9a4a9b4f3b5187de0d608c964a77e9044222b0b
Yoiner Duran <duranyoiner@gmail.com>
2023-10-12 17:03:28 -0500

    production v5

--- config/settings.py
+++ config/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
