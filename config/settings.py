803dc662d46e80886f54daddaa64adece0b6e02b
Yoiner Duran <duranyoiner@gmail.com>
2023-10-12 16:12:31 -0500

    production v3

--- config/settings.py
+++ config/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
