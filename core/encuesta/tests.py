530b9d2e0ef7d1f3ae7d2937d6a49063ed22bf99
Yoiner Duran <duranyoiner@gmail.com>
2023-11-06 23:11:34 -0500

    db prod y local

--- core/encuesta/tests.py
+++ core/encuesta/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
-
+from core.encuesta.models import Pregunta
 # Create your tests here.
+
+preguntas_local=Pregunta.objects.using('default').all()
+for pregunta in preguntas_local:
+  Pregunta.objects.using('production').create(pregunta=pregunta.pregunta)
